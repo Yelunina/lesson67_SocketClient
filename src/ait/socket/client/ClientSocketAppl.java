5c07431b192de53383aabe800f22bebf8253c39e
Iryna Yelunina <lucky.asterisk@gmail.com>
2023-12-14 13:01:48 +0100

    init

---
+++ src/ait/socket/client/ClientSocketAppl.java
@@ -0,0 +1,33 @@
+package ait.socket.client;
+
+import java.io.*;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class ClientSocketAppl {
+    public static void main(String[] args) {
+        String serverHost = "127.0.0.1";//localhost
+        int port = 9000;
+        try (Socket socket = new Socket(serverHost, port)){
+            InputStream inputStream = socket.getInputStream();
+            OutputStream outputStream =socket.getOutputStream();
+            PrintWriter socketWriter = new PrintWriter(outputStream);
+            BufferedReader socketReader = new BufferedReader(new InputStreamReader(inputStream));
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("Enter your message or type exit for quit");
+            String message = br.readLine();
+            while (!"exit".equalsIgnoreCase(message)){
+                socketWriter.println(message);
+                socketWriter.flush();
+                String response = socketReader.readLine();
+                System.out.println(response);
+                System.out.println("Enter your message or type exit for quit");
+                message = br.readLine();
+            }
+        } catch (UnknownHostException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
